10ea7da0bd9f66bfba080834af6f47bcfb68993c
Tamara A <akira-49-q@yandex.ru>
2022-02-25 08:27:58 +0200

    refactor: Icon componenet

--- components/Icon.js
+++ components/Icon.js
@@ -1,8 +1,6 @@
 export class Icon {
   constructor(id) {
     this._id = id;
-    this._iconType;
-    this._iconId;
     this._symbolsData = {
       1: '+',
       2: '-',
@@ -15,32 +13,56 @@ export class Icon {
 
   /** */
   insertIcon(node) {
-    this._defineIcon();
     const icon = document.createElement('div');
     icon.setAttribute('class', 'icon');
+
+    const data = this._defineIcon();
+    let iconNode;
     
-    if (this._iconType == 'icon') {
-      let elem = document.createElement('img');
-      elem.setAttribute('src', `./images/image_${this._iconId}.png`);
-      elem.setAttribute('alt', `icon ${this._iconId}`);
-      icon.append(elem);
-    } else if (this._iconType == 'symbol') {
-      icon.innerText = this._symbolsData[this._iconId];
+    if (data.type == 'icon') {
+      iconNode = this._handleIconType(icon, data.id);
+    } else if (data.type == 'symbol') {
+      iconNode = this._handleSymbolType(icon, data.id);
     } else {
-      icon.innerText = this._iconId;
+      iconNode = this._handleNumberType(icon, data.id);
     }
 
     document
-        .querySelector(`#expression_${node}`)
-        .querySelector('.expression')
-        .append(icon);
+    .querySelector(`#expression_${node}`)
+    .querySelector('.expression')
+    .append(iconNode);
   }
 
   /** */
   _defineIcon() {
     const arr = this._id.split('-');
-    this._iconType = arr[0];
-    this._iconId = arr[1];
+
+    return {
+      type: arr[0],
+      id: arr[1],
+    }
+  }
+
+  /** */
+  _handleIconType(iconNode, id) {
+    let elem = document.createElement('img');
+    elem.setAttribute('src', `./images/image_${id}.png`);
+    elem.setAttribute('alt', `icon ${id}`);
+
+    iconNode.append(elem);
+    return iconNode;
+  }
+
+  /** */
+  _handleSymbolType(iconNode, id) {
+    iconNode.innerText = this._symbolsData[id];
+    return iconNode;
+  }
+
+  /** */
+  _handleNumberType(iconNode, id) {
+    iconNode.innerText = id;
+    return iconNode;
   }
 
 };
